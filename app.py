7ea2593b8dc39bd9542629d903848a69fbe9fcac
RadcliffeC <ck.radcliffe@gmail.com>
2025-10-27 20:42:53 -0500

    A small script used to make and display csv files as different types of graphs in Pyhton using seaborn, pandas, and matplotlib.

---
+++ app.py
@@ -0,0 +1,32 @@
+import seaborn as sns
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def makeplot(data, gtype):
+    filepath = f'HotfireData/{data}'
+    df = pd.read_csv(filepath)
+    x = 'Time'
+    y = 'Value'
+    if data == 'combined_cf.csv' or data == 'fuel_coldflow.csv':
+        y = 'Pressure'
+
+    match gtype:
+        case 'scatter':
+            sns.scatterplot(x=x, y=y, data=df)
+        case 'line':
+            sns.lineplot(x=x, y=y, data=df)
+        case 'bar':
+            sns.barplot(x=x, y=y, data=df)
+        case 'hist':
+            sns.histplot(x=x, y=y, data=df)
+        case _:
+            print("Invalid gtype")
+
+    plt.draw()
+    plt.show()
+
+if __name__ == '__main__':
+    csv = input("Enter csv file\n")
+    gtype = input("Enter gtype, scatter, line, bar, hist\n")
+
+    makeplot(csv, gtype)
